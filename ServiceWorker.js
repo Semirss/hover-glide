f50cd00b57918887fe39e72bc025c842e5c42474
Semir_S <149691609+Semirss@users.noreply.github.com>
2025-09-19 07:55:50 +0300

    new

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Havoc Glide-0.1";
 const contentToCache = [
-    "Build/Build.loader.js",
-    "Build/Build.framework.js.unityweb",
-    "Build/Build.data.unityweb",
-    "Build/Build.wasm.unityweb",
+    "Build/hover glide.loader.js",
+    "Build/hover glide.framework.js.unityweb",
+    "Build/hover glide.data.unityweb",
+    "Build/hover glide.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
